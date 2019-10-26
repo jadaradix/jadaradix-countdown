97792c328b2d3b295e57ba0d7fd2eddd06f2de5e
James Garner <j@jada.io>
2019-10-26 13:29:23 +0100

    date configurable with environment variables

--- modules/getDays/index.js
+++ modules/getDays/index.js
@@ -1,9 +1,13 @@
-const oneDay = 24 * 60 * 60 * 1000
+const ONE_DAY = 24 * 60 * 60 * 1000
+
+const YEAR = typeof process.env.YEAR === 'string' ? parseInt(process.env.YEAR, 10) : new Date().getFullYear()
+const MONTH = typeof process.env.MONTH === 'string' ? parseInt(process.env.MONTH, 10) : 11
+const DAY = typeof process.env.DAY === 'string' ? parseInt(process.env.DAY, 10) : 30
 
 const getDays = () => {
   const firstDate = new Date()
-  const secondDate = new Date(new Date().getFullYear(), 11, 30) // year, month, day
-  return Math.floor(Math.abs((firstDate.getTime() - secondDate.getTime()) / oneDay))
+  const secondDate = new Date(YEAR, MONTH, DAY)
+  return Math.floor(Math.abs((firstDate.getTime() - secondDate.getTime()) / ONE_DAY))
 }
 
 module.exports = getDays
